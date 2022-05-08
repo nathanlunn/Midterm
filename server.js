14691479b2d25524a1d10652fa1465c7b535632e
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-05-08 23:24:53 +0000

    add cookie-session and body-parser as dependencies

--- server.js
+++ server.js
@@ -13,11 +13,23 @@ const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ["thisisalongsecretkeyithink"],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
+
+// cookies session to grab user_id from cookie
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -37,11 +49,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+// const single_itemRoutes = require('./routes/single_item');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+// app.use('/', single_itemRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
